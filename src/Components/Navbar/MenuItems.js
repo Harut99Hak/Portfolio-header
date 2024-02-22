f25ab99f3d346f53af91e6c9b3492b3711ed0fc7
Harut99Hak <123800367+Harut99Hak@users.noreply.github.com>
2024-02-22 16:30:49 +0400

    first commit

---
+++ src/Components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: "Home",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "About",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "Works",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "Contact Me ",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "Sign in",
+        url: "#",
+        cName: "nav-links-mobile"
+    },
+]
